5e6daee01dd35c0786da1f48c367ccc614a02f92
POSITIVO18 <angel.22aac06@gmail.com>
2024-04-30 18:23:49 -0300

    meu primeiro commit no paycharm

---
+++ graficos.py
@@ -0,0 +1,8 @@
+import matplotlib.pyplot as plt
+nome = ['javid','alenri','samuel','kesia','larissa','maria','aloizio']
+idade = [17,16,17,17,17,17,16]
+plt.plot(nome,idade)
+plt.title('grafico')
+plt.xlabel('nome')
+plt.ylabel('idade')
+plt.show()
---
+++ index.py
@@ -0,0 +1,6 @@
+for numero in range(2,21,2):
+  print(numero)
+
+cores = ["azul", "verde", "roxo", "vermelho", "amarelo"]
+for cor in cores:
+      print(cor)
